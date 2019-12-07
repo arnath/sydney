6a3b2bb7924f2519bffaae5f08319e926330a22a
arnath <prakash.vijay@gmail.com>
2019-12-07 21:15:06 +0100

    Initial commit.

---
+++ Sydney.Core/HttpService.cs
@@ -0,0 +1,54 @@
+﻿namespace Sydney.Core
+{
+    using System;
+    using System.Net;
+
+    public class HttpService : IDisposable
+    {
+        private readonly HttpListener httpListener;
+
+        private bool running = false;
+
+        public HttpService()
+        {
+            this.httpListener = new HttpListener();
+        }
+
+        public void Start()
+        {
+            this.running = true;
+            this.httpListener.Start();
+            while (this.running)
+            {
+                HttpListenerContext context = this.httpListener.GetContext();
+                // Dispatch the request using a task.
+            }
+        }
+
+        public void Stop()
+        {
+            this.running = false;
+            if (this.httpListener.IsListening)
+            {
+                this.httpListener.Stop();
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.httpListener != null)
+                {
+                    this.httpListener.Close();
+                }
+            }
+        }
+    }
+}
